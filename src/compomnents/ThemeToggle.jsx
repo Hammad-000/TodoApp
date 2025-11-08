4a7c719704f584e37a893c0e2c03587388e98e43
Hammad Ahmed <syedhammad283@gmail.com>
2025-11-08 08:18:59 +0500

    themefix

--- src/compomnents/ThemeToggle.jsx
+++ src/compomnents/ThemeToggle.jsx
@@ -5,11 +5,7 @@ import darkLogo from "../images/edit-3-svgrepo-com (2).svg";
 function ThemeToggle({ theme, setTheme }) {
   return (
     <div className="theme-toggle">
-      <img
-        src={theme === "dark" ? darkLogo : lightLogo}
-        alt="Theme logo"
-        className="theme-logo"
-      />
+    
       <input
         type="checkbox"
         id="checkbox"
